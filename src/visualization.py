425ce8d28c51cd18de0cd61a213b56bb15ae4482
vlad_pushak <sellnewacc8@gmail.com>
2026-05-13 11:38:53 +0300

    Add visualization script

---
+++ src/visualization.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def create_plots():
+    print("Цей скрипт призначений для побудови графіків розподілу закладів.")
+    # Тут зазвичай йде код для побудови гістограм
+    # plt.show()
+
+if __name__ == "__main__":
+    create_plots()
